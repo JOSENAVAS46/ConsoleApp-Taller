c0421e73cbaa1c274825eb43f58f245e3be4737c
JOSE NAVAS <jose.navaso@ug.edu.ec>
2023-06-26 19:51:49 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Models/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+   
+    
+        public class AppDbContext : DbContext
+        {
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-HNU4CB9;Database=ConsoleAppDB;Trusted_Connection=True;");
+        }
+
+
+    }
+}
